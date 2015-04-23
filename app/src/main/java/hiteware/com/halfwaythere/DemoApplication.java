2ed1fe020cbe8342be960cbf259e6c18c8b3abd2
Jason Hite <hite77@gmail.com>
2015-04-23 15:42:58 -0400

    First commit on this branch -- able to extend Application

---
+++ app/src/main/java/hiteware/com/halfwaythere/DemoApplication.java
@@ -0,0 +1,13 @@
+package hiteware.com.halfwaythere;
+
+import android.app.Application;
+
+/**
+ * Created by jasonhite on 4/23/15.
+ */
+public class DemoApplication extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/test/java/TestDemoApplication.java
@@ -0,0 +1,19 @@
+import hiteware.com.halfwaythere.DemoApplication;
+
+/**
+ * Created by jasonhite on 4/23/15.
+ */
+public class TestDemoApplication extends DemoApplication {
+
+//    @Override
+//    protected ArrayList<Object> getModules() {
+//        ArrayList<Object> modules = super.getModules();
+//        modules.add(new TestModule());
+//        return modules;
+//    }
+//
+//    public static <T> T injectMocks(T object) {
+//        TestMyApplication app = (TestMyApplication) Robolectric.application;
+//        return app.inject(object);
+//    }
+}
