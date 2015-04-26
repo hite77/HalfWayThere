343fb235685c8de2bbea982c368929e9600cbf28
Jason Hite <hite77@gmail.com>
2015-04-26 16:49:32 -0400

    trying on phone to see if speed is accurate when standing still, if so don't update when you are not moving

--- app/src/main/java/hiteware/com/halfwaythere/MainActivityFragment.java
+++ app/src/main/java/hiteware/com/halfwaythere/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package hiteware.com.halfwaythere;
 
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -8,9 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
+import java.util.Vector;
 
 import javax.inject.Inject;
 
@@ -20,6 +24,8 @@ import static hiteware.com.halfwaythere.Conversion.distanceInMiles;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment implements LocationListener{
+
+    private List<String> locations = new Vector<>();
 
     private boolean firstLocation = false;
     private double distance = 0;
@@ -51,8 +57,58 @@ public class MainActivityFragment extends Fragment implements LocationListener{
         ((DemoApplication)getActivity().getApplication()).inject(getActivity(), this);
 
         initializeListeners();
+
+        if (view != null && view.findViewById(R.id.email_button) != null) {
+            Button button = (Button) view.findViewById(R.id.email_button);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(), "Email Clicked", Toast.LENGTH_SHORT).show();
+                    emailIntent();
+                }
+            });
+        }
+
+        if (view != null && view.findViewById(R.id.Clear_Button) != null)
+        {
+            Button button = (Button) view.findViewById(R.id.Clear_Button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(), "Clear Clicked", Toast.LENGTH_SHORT).show();
+                    locations.clear();
+                }
+            });
+        }
+
         return view;
     }
+
+    // spike code -- to email coordinates and estimated distance travelled
+    // don't know what format is needed.
+    // keep it simple -- get coordinates, store them, also clear them out when clear button pressed.
+
+    private void emailIntent()
+    {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"hite77@gmail.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+
+        String output = new String();
+        for (String line: locations)
+        {
+            output = output+ line;
+        }
+        i.putExtra(Intent.EXTRA_TEXT   , output);
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     public void onLocationChanged(Location location) {
@@ -61,7 +117,19 @@ public class MainActivityFragment extends Fragment implements LocationListener{
                                         current_location.getLatitude(), current_location.getLongitude());
             TextView outputView = (TextView) getView().findViewById(R.id.distance_value);
             outputView.setText(String.format( "%.2f", distance )+" miles");
+
+            float speed = location.getSpeed();
+            TextView speedView = (TextView) getView().findViewById(R.id.SpikeSpeedText);
+            speedView.setText(String.format("%.2f", speed));
+
+            String output = new String(location.getLatitude() + "," + location.getLongitude() + " distance:"+distance+ " speed:"+ speed+"\n");
+            locations.add(output);
+
+            ((TextView)getView().findViewById(R.id.SpikeLatText)).setText(String.format("%.6f", location.getLatitude()));
+            ((TextView)getView().findViewById(R.id.SpikeLongText)).setText(String.format("%.6f", location.getLongitude()));
+
         }
+
         current_location = location;
         firstLocation = true;
     }
