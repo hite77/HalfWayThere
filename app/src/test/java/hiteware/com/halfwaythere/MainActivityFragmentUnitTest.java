a893501b1d86c0bbf52aa991366a74fcec4b7058
Jason Hite <hite77@gmail.com>
2015-06-19 13:53:40 -0400

    Some more refactors, splitting out a test, and removing common

--- app/src/test/java/hiteware/com/halfwaythere/MainActivityFragmentUnitTest.java
+++ app/src/test/java/hiteware/com/halfwaythere/MainActivityFragmentUnitTest.java
@@ -18,7 +18,6 @@ import org.robolectric.util.ActivityController;
 import java.util.List;
 
 import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNotSame;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.atLeastOnce;
@@ -35,6 +34,13 @@ import static org.robolectric.Shadows.shadowOf;
 public class MainActivityFragmentUnitTest {
     private MainActivity CreatedActivity;
 
+    private void sendBroadcast(String action, String extra, int value) {
+        Intent broadcast = new Intent();
+        broadcast.setAction(action);
+        broadcast.putExtra(extra, value);
+        CreatedActivity.sendBroadcast(broadcast);
+    }
+
     @Test
     public void whenTheAppIsRunningTheServiceWillBeStarted() {
         ActivityController controller = Robolectric.buildActivity(MainActivity.class).create().start();
@@ -49,11 +55,7 @@ public class MainActivityFragmentUnitTest {
     public void whenBroadcastOfStepsIsReceivedThenStepsAreDisplayed() {
         CreatedActivity = Robolectric.buildActivity(MainActivity.class).create().postResume().get();
 
-        Intent broadcastSteps = new Intent();
-        broadcastSteps.setAction(StepService.ACTION_STEPS_OCCURRED);
-        int expected = 45;
-        broadcastSteps.putExtra(StepService.STEPS_OCCURRED, expected);
-        CreatedActivity.sendBroadcast(broadcastSteps);
+        sendBroadcast(StepService.ACTION_STEPS_OCCURRED, StepService.STEPS_OCCURRED, 45);
 
         assertThat(((TextView) CreatedActivity.findViewById(R.id.step_value)).getText().toString(), equalTo("45"));
     }
@@ -64,24 +66,17 @@ public class MainActivityFragmentUnitTest {
 
         CreatedActivity = Robolectric.buildActivity(MainActivity.class).create().start().resume().postResume().get();
 
-        Intent broadcastSteps = new Intent();
-        broadcastSteps.setAction(StepService.ACTION_STEPS_OCCURRED);
         int expected = 45;
-        broadcastSteps.putExtra(StepService.STEPS_OCCURRED, expected);
-        CreatedActivity.sendBroadcast(broadcastSteps);
-
-        Intent broadcastGoal = new Intent();
-        broadcastGoal.setAction(StepService.ACTION_GOAL_CHANGED);
         int expectedGoal = 14000;
-        broadcastGoal.putExtra(StepService.GOAL_SET, expectedGoal);
-        CreatedActivity.sendBroadcast(broadcastGoal);
+        sendBroadcast(StepService.ACTION_STEPS_OCCURRED, StepService.STEPS_OCCURRED, expected);
+        sendBroadcast(StepService.ACTION_GOAL_CHANGED, StepService.GOAL_SET, expectedGoal);
 
         ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
 
-        View expecectedCircularProgress = CreatedActivity.findViewById(R.id.circularProgressWithHalfWay);
+        View expectedCircularProgress = CreatedActivity.findViewById(R.id.circularProgressWithHalfWay);
 
         ProgressUpdateInterface progressUpdate = ((TestInjectableApplication) RuntimeEnvironment.application).testModule.provideProgressUpdate();
-        verify(progressUpdate, times(1)).SetCircularProgress((CircularProgressWithHalfWay) expecectedCircularProgress);
+        verify(progressUpdate, times(1)).SetCircularProgress((CircularProgressWithHalfWay) expectedCircularProgress);
         verify(progressUpdate, atLeastOnce()).SetSteps(expected);
         verify(progressUpdate, atLeastOnce()).SetGoal(expectedGoal);
     }
@@ -90,11 +85,7 @@ public class MainActivityFragmentUnitTest {
     public void whenBroadcastOfGoalIsReceivedThenGoalIsDisplayed() {
         CreatedActivity = Robolectric.buildActivity(MainActivity.class).create().postResume().get();
 
-        Intent broadcastGoal = new Intent();
-        broadcastGoal.setAction(StepService.ACTION_GOAL_CHANGED);
-        int expected = 14000;
-        broadcastGoal.putExtra(StepService.GOAL_SET, expected);
-        CreatedActivity.sendBroadcast(broadcastGoal);
+        sendBroadcast(StepService.ACTION_GOAL_CHANGED, StepService.GOAL_SET, 14000);
 
         assertThat(((TextView) CreatedActivity.findViewById(R.id.goal_value)).getText().toString(), equalTo("14000"));
     }
@@ -102,53 +93,60 @@ public class MainActivityFragmentUnitTest {
     @Test
     public void whenHalfWayButtonIsClickedThenBroadcastIsSentToService()
     {
-        CreatedActivity = Robolectric.buildActivity(MainActivity.class).create().postResume().get();
+        CreatedActivity = Robolectric.buildActivity(MainActivity.class).create().start().resume().postResume().get();
+
+        sendBroadcast(StepService.ACTION_STEPS_OCCURRED, StepService.STEPS_OCCURRED, 1000);
+        sendBroadcast(StepService.ACTION_GOAL_CHANGED, StepService.GOAL_SET, 14000);
+
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
+
         StepServiceUnitTestReceiver testReceiver = new StepServiceUnitTestReceiver();
         CreatedActivity.registerReceiver(testReceiver, new IntentFilter(StepService.ACTION_HALF_WAY_SET));
 
         CreatedActivity.findViewById(R.id.HalfWayToggle).performClick();
         ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
 
-        assertNotSame(testReceiver.getActualHalfWay(), -1);
+        assertThat(testReceiver.getActualHalfWay(), equalTo(7500));
     }
 
     @Test
-    public void whenHalfWayButtonIsClickedThenCountIsUpdatedForHalfWayAndProgressUpdateIsUpdated()
+    public void whenHalfWayButtonIsClickedThenProgressUpdateIsUpdated()
     {
         ((TestInjectableApplication) RuntimeEnvironment.application).setMock();
+        ProgressUpdateInterface progressUpdate = ((TestInjectableApplication) RuntimeEnvironment.application).testModule.provideProgressUpdate();
 
         CreatedActivity = Robolectric.buildActivity(MainActivity.class).create().start().resume().postResume().get();
 
-        Intent broadcastSteps = new Intent();
-        broadcastSteps.setAction(StepService.ACTION_STEPS_OCCURRED);
-        int steps = 1000;
-        broadcastSteps.putExtra(StepService.STEPS_OCCURRED, steps);
-        CreatedActivity.sendBroadcast(broadcastSteps);
-
-        Intent broadcastGoal = new Intent();
-        broadcastGoal.setAction(StepService.ACTION_GOAL_CHANGED);
-        int goal = 14000;
-        broadcastGoal.putExtra(StepService.GOAL_SET, goal);
-        CreatedActivity.sendBroadcast(broadcastGoal);
+        sendBroadcast(StepService.ACTION_STEPS_OCCURRED, StepService.STEPS_OCCURRED, 4653);
+        sendBroadcast(StepService.ACTION_GOAL_CHANGED, StepService.GOAL_SET, 5778);
 
         ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
 
-        StepServiceUnitTestReceiver testReceiver = new StepServiceUnitTestReceiver();
-        CreatedActivity.registerReceiver(testReceiver, new IntentFilter(StepService.ACTION_HALF_WAY_SET));
+        CreatedActivity.findViewById(R.id.HalfWayToggle).performClick();
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
 
-        ProgressUpdateInterface progressUpdate = ((TestInjectableApplication) RuntimeEnvironment.application).testModule.provideProgressUpdate();
+        verify(progressUpdate, atLeastOnce()).SetHalfWay(5215);
+    }
+
+    @Test
+    public void whenHalfWayButtonIsClickedThenCountIsUpdatedForHalfWay()
+    {
+        CreatedActivity = Robolectric.buildActivity(MainActivity.class).create().start().resume().postResume().get();
+
+        sendBroadcast(StepService.ACTION_STEPS_OCCURRED, StepService.STEPS_OCCURRED, 1333);
+        sendBroadcast(StepService.ACTION_GOAL_CHANGED, StepService.GOAL_SET, 2000);
+
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
 
         CreatedActivity.findViewById(R.id.HalfWayToggle).performClick();
+
         ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
 
         TextView halfWayValue = (TextView) CreatedActivity.findViewById(R.id.HalfWayValue);
 
-        assertThat(testReceiver.getActualHalfWay(), equalTo(1000 + (13000 / 2)));
-        assertThat(halfWayValue.getText().toString(), equalTo("7500"));
-
-        verify(progressUpdate, atLeastOnce()).SetHalfWay(7500);
+        assertThat(halfWayValue.getText().toString(), equalTo("1666"));
     }
-//
+
 //    @Test //TODO: whenHalfWayButtonIsClickedAndIsInvalidFromProgressUpdateThenNoBroadcast
 //    public void whenHalfWayButtonIsClickedAndIsInvalidFromProgressUpdateThenNoBroadcast()
 //    {
